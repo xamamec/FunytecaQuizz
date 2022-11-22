9dc726617f6a9af91fc49042539df09366c372ad
xamamec <63166911+xamamec@users.noreply.github.com>
2022-11-21 23:50:26 -0400

    login com o google

--- app/src/main/java/com/example/funytecaquizz/LoginActivity.java
+++ app/src/main/java/com/example/funytecaquizz/LoginActivity.java
@@ -16,22 +16,32 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView forgotPassB, cadastroB;
     private EditText email, pass;
     private Button loginB;
-    private RelativeLayout gCadastroB;
+    private RelativeLayout gLoginB;
     private FirebaseAuth mAuth;
     private Dialog progressDialog;
     private TextView dialogText;
+    private GoogleSignInClient mGoogleSignInClient;
+    private int RC_SIGN_IN = 104;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +53,7 @@ public class LoginActivity extends AppCompatActivity {
         loginB = findViewById(R.id.loginB);
         forgotPassB = findViewById(R.id.forgot_pass);
         cadastroB = findViewById(R.id.cadastroB);
-        gCadastroB= findViewById(R.id.g_cadastroB);
+        gLoginB= findViewById(R.id.g_LoginB);
 
         progressDialog = new Dialog(LoginActivity.this);
         progressDialog.setContentView(R.layout.dialog_layout);
@@ -55,10 +65,12 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth =  FirebaseAuth.getInstance();
 
-//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//                .requestIdToken(getString(R.string.default_web_client_id))
-//                .requestEmail()
-//                .build();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
 
 
         loginB.setOnClickListener(new View.OnClickListener() {
@@ -81,7 +93,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        gCadastroB.setOnClickListener(new View.OnClickListener() {
+        gLoginB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 googleSignIn();
@@ -141,9 +153,64 @@ public class LoginActivity extends AppCompatActivity {
 
     private void googleSignIn()
     {
-
-
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                  //      Log.d(TAG, "FirebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+
+            } catch (ApiException e){
+                //Log.w(TAG,"Login com o google falhou", e);
+                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+
+        progressDialog.show();
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(LoginActivity.this,"Login com o google foi um sucesso", Toast.LENGTH_SHORT).show();
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            progressDialog.dismiss();
+
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            LoginActivity.this.finish();
+
+
+                        } else {
+
+                            progressDialog.dismiss();
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+
 
 
 }
